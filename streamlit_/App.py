c624e21ec6e7cae43774b8c7039f4b5b7380ec63
JinsicLee <119823453+JinsicLee@users.noreply.github.com>
2022-12-13 15:14:20 +0900

    ㄱ

--- streamlit_/App.py
+++ streamlit_/App.py
@@ -9,14 +9,18 @@ import pandas as pd
 
 st.title(" <조> 추첨 페이지")
 st.header(" 여러분의 참여를 환영합니다 :) ")
-#st.image("./no_gs.jpg")
+# st.image("./lucky/no_gs.jpg")
+# st.image("https://i.imgur.com/RzqvApm.jpg")
 
 # https://docs.streamlit.io/library/cheatsheet
 # 추첨 대상인 13명의 이름을 넣을 수 있는 text_input
 # 3 x 4 (row, col)
 # 열을 배치하는 메소드
+
 tabs = st.tabs(['참가자','조'])
+
 #0번째 탭에 columns(열)을 삽입
+
 columns = tabs[0].columns(4) # 화면을 열로 나누어서 배치
 # 가로 4개의 열 -> columns = [col1, col2, col3, col4]
 # col1, col2, col3, col4
@@ -32,11 +36,11 @@ for idx, col in enumerate(columns): #열의 위치
             key=f"n{idx+1 + idx2 * 4}"
             )#4번 호출됨
 
-columns2 = tabs[1].columns(4)
 #2번째 조
 #columns -> columns2 , taps[0], -> taps[1]
+columns2 = tabs[1].columns(4)
 
-for idx, col in enumerate(columns2): #열의 위치
+for idx, col in enumerate(columns4): #열의 위치
     # 이중 For문
     # col.text_input(f"조 추첨 대상 {idx+1}", key=idx)
     for idx2 in range(4):
@@ -44,11 +48,12 @@ for idx, col in enumerate(columns2): #열의 위치
         # col 안에 메소드를 통하여 element를 생성
         col.text_input(
             f"조 목록 {idx+1 + idx2 * 4}", 
-            key=f"n{idx+1 + idx2 * 4}"
+            key=f"g{idx+1 + idx2 * 4}"
             )#4번 호출됨
+
 # <추첨 버튼>
 # 13개의 짝을 지어서 표시해줄 그래픽
-if st.button('추첨시작^^'):
+if st.button('추첨시작^^ㅋ'):
 
     # 13명이 소속될 조 이름을 넣을 위치
     #st.write(st.session_state)
@@ -62,17 +67,17 @@ if st.button('추첨시작^^'):
     # contains, find  * str관련 메소드 사용할 수 있게 함
     n_idx = ss2.index.str.contains('n')
     n_data = ss2[n_idx]
-    st.write(n_data)
+    #st.write(n_data)
 
     g_idx = ss2.index.str.contains('g')
     g_data = ss2[g_idx]
-    st.write(g_data)
+    #st.write(g_data)
 
     # n_data를 섞여줄 것임 (비복원으로)
     n_rd = np.random.choice(n_data, len(n_data), replace=False)
-    st.write(n_rd)
+    #st.write(n_rd)
     g_rd = np.random.choice(g_data, len(g_data), replace=False)
-    st.write(g_rd)
+    #st.write(g_rd)
 
     # 2. df 형태로 정리
     df = pd.DataFrame({
