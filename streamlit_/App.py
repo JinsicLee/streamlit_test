0ef078e6a622e33e24335121d09190a1cc0d92ae
JinsicLee <119823453+JinsicLee@users.noreply.github.com>
2022-12-13 14:16:38 +0900

    r

--- streamlit_/App.py
+++ streamlit_/App.py
@@ -25,8 +25,7 @@ for idx, col in enumerate(columns): #열의 위치
         # key가 겹치면 안 됨
         # col 안에 메소드를 통하여 element를 생성
         col.text_input
-            (f"조 추첨 대상 {idx+1 + idx2 * 4}", 
-            key=f"{idx+1 + idx2 * 4}")
+            (f"조 추첨 대상 {idx+1 + idx2 * 4}", key=f"{idx+1 + idx2 * 4}")
 
 # 13명이 소속될 조 이름을 넣을 위치
 st.write(st.session_state)
