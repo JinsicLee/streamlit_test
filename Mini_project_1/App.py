ad9194a8c3c76d726e78195acefd4bdec7619123
JinsicLee <119823453+JinsicLee@users.noreply.github.com>
2022-12-13 18:43:04 +0900

    r

--- Mini_project_1/App.py
+++ Mini_project_1/App.py
@@ -20,7 +20,7 @@ st.write(df)
 # df.describe()
 # df.info()
 
- df.plot(x='발생일', y='모기지수(수변부)')
+# df.plot(x='발생일', y='모기지수(수변부)')
 # df.plot(x='발생일', y='모기지수(주거지)')
 # df.plot(x='발생일', y='모기지수(공원)')
 
