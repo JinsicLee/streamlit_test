701653d78667502d63c4b19f6eb1e0bc8ffe22d0
JinsicLee <119823453+JinsicLee@users.noreply.github.com>
2022-12-14 11:52:39 +0900

    r

--- Mini_project_1/App.py
+++ Mini_project_1/App.py
@@ -13,6 +13,7 @@ for percent_complete in range(100):
     my_bar.progress(percent_complete + 1)
 
 st.error('지금 제작 중 입니다', icon="🚨")
+
     if st.button('Button'):
         st.write('조금만 기다려주세요')
         st.snow()
