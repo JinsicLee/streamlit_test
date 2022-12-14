7ed837c2f7951f87eb537c5359c386e311fc6f76
JinsicLee <119823453+JinsicLee@users.noreply.github.com>
2022-12-14 11:39:50 +0900

    ㄱ

--- Mini_project_1/App.py
+++ Mini_project_1/App.py
@@ -8,7 +8,7 @@ import time
 
 st.write("Loading ....")
 my_bar = st.progress(0)
-for percent_complete in range(100):
+for percent_complete in range(1000):
     time.sleep(0.1)
     my_bar.progress(percent_complete + 1)
 
