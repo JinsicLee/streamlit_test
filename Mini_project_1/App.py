31a4cdc1db226028bfd81a3c7abcb60429a901aa
JinsicLee <119823453+JinsicLee@users.noreply.github.com>
2022-12-14 11:37:35 +0900

    ㄱ

--- Mini_project_1/App.py
+++ Mini_project_1/App.py
@@ -12,34 +12,34 @@ for percent_complete in range(100):
     time.sleep(0.1)
     my_bar.progress(percent_complete + 1)
 
-#import plotly.express as px
+# #import plotly.express as px
 
-# 서울시의 모기 파악하기
-## 모기예보제와 일자별 날씨 데이터를 통한 모기 발생 빈도 변화 파악
+# # 서울시의 모기 파악하기
+# ## 모기예보제와 일자별 날씨 데이터를 통한 모기 발생 빈도 변화 파악
 
-st.image("https://news.seoul.go.kr/welfare/files/2020/02/62cfc9f3f36041.41905113-1086x1536.jpg")
-st.write("http://data.seoul.go.kr/dataList/OA-13285/S/1/datasetView.do#")
+# st.image("https://news.seoul.go.kr/welfare/files/2020/02/62cfc9f3f36041.41905113-1086x1536.jpg")
+# st.write("http://data.seoul.go.kr/dataList/OA-13285/S/1/datasetView.do#")
 
-# UTF-8 / CP-949
-# https://seong6496.tistory.com/269
-st.write("Read Data List 1 >>")
+# # UTF-8 / CP-949
+# # https://seong6496.tistory.com/269
+# st.write("Read Data List 1 >>")
 
-df = pd.read_csv('./Mini_project_1/mosquito.csv', encoding='cp949')
-df['발생일']=pd.to_datetime(df['모기지수 발생일'], infer_datetime_format=True)
-df = df[:2517]
-st.write(df)
+# df = pd.read_csv('./Mini_project_1/mosquito.csv', encoding='cp949')
+# df['발생일']=pd.to_datetime(df['모기지수 발생일'], infer_datetime_format=True)
+# df = df[:2517]
+# st.write(df)
 
-#st.write("Loading ....")
+# #st.write("Loading ....")
 
-# df.describe()
-# df.info()
-# lp_1 = sns.lineplot(data=df, x='모기지수 발생일',y='모기지수(수변부)')
-# # df.plot(x='발생일', y='모기지수(수변부)')
-# # df.plot(x='발생일', y='모기지수(주거지)')
-# # df.plot(x='발생일', y='모기지수(공원)')
-# plt.show()
+# # df.describe()
+# # df.info()
+# # lp_1 = sns.lineplot(data=df, x='모기지수 발생일',y='모기지수(수변부)')
+# # # df.plot(x='발생일', y='모기지수(수변부)')
+# # # df.plot(x='발생일', y='모기지수(주거지)')
+# # # df.plot(x='발생일', y='모기지수(공원)')
+# # plt.show()
 
-# sns.lineplot(data=df, x="발생일", y="모기지수(수변부)")
-# plt.show()
-# sns.lineplot(data=df, x="발생일", y="모기지수(주거지)")
-# sns.lineplot(data=df, x="발생일", y="모기지수(공원)")
+# # sns.lineplot(data=df, x="발생일", y="모기지수(수변부)")
+# # plt.show()
+# # sns.lineplot(data=df, x="발생일", y="모기지수(주거지)")
+# # sns.lineplot(data=df, x="발생일", y="모기지수(공원)")
