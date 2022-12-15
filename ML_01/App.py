65f82aff102bcd4de765ac65ddc748b0c689c394
JinsicLee <119823453+JinsicLee@users.noreply.github.com>
2022-12-15 17:17:50 +0900

    r

--- ML_01/App.py
+++ ML_01/App.py
@@ -12,3 +12,10 @@ model_path = f"{os.path.dirname(os.path.abspath(__file__))}/model.pkl"
 model = joblib.load(model_path)
 st.write("## 선형 회귀 모델")
 st.write(pd.Series(model.coef_, index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"]))
+
+#age = 나이
+age = st.number_inpit(
+    label='나이',
+    step=1,
+    value=30,
+)
