57b558f6a143d37075bed9d00090eecdece8dc09
JinsicLee <119823453+JinsicLee@users.noreply.github.com>
2022-12-15 17:08:18 +0900

    r

--- ML_01/App.py
+++ ML_01/App.py
@@ -1,6 +1,11 @@
 # streamlit 라이브러리 호출
 import streamlit as st
 import pandas as pd
-#Test_1
+
 df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/data/insurance.csv')
 st.write(df)
+
+import joblib
+model = joblib.load('./ml_model/app.py')
+model_info = pd.Series(model.coef_, index = df.drop['expenses'].columns)
+st.write(model_info)
